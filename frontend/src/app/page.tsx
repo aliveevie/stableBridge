d1ce3072e52bd3e046ecb2d9d9035a643928399b
Ibrahim <aliveibi080@gmail.com>
2024-07-25 10:25:05 +0100

    Update files

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/components/header";
-
+import { Hero1 } from "@/components/hero1";
+import { Hero2 } from "@/components/hero2";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero1 />
+      <Hero2 />
     </>
   );
 }
