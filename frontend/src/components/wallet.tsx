11da37dc50d9e40f46397bfbb1d6d2183cf806bb
Ibrahim <aliveibi080@gmail.com>
2024-09-19 10:29:18 +0100

    Update files

--- frontend/src/components/wallet.tsx
+++ frontend/src/components/wallet.tsx
@@ -1,17 +1,27 @@
 import { useAuth } from '@micro-stacks/react';
 import { Button } from './ui/button';
- 
+import { ClientProvider } from '@micro-stacks/react'; 
+
+
 export const WalletConnectButton = () => {
   const { openAuthRequest, isRequestPending, signOut, isSignedIn } = useAuth();
   const label = isRequestPending ? 'Loading...' : isSignedIn ? 'Sign out' : 'Connect wallet';
   return (
-    <Button className="hidden md:inline-flex"
-      onClick={async () => {
-        if (isSignedIn) await signOut();
-        else await openAuthRequest();
-      }}
-    >
-      {label}
+    <>
+      <ClientProvider 
+        appName="Nextjs + Microstacks"
+        appIconUrl="/vercel.png"
+      >
+        <Button className="hidden md:inline-flex"
+        onClick={async () => {
+          if (isSignedIn) await signOut();
+          else await openAuthRequest();
+        }}
+      >
+        {label}
     </Button>
+      </ClientProvider>
+    </>
+  
   );
 };
