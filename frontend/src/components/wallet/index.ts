6fc70e729a8ecbf10500d7ebafd51270b1c135d7
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-12-23 21:37:06 +0100

    feat(wallet): add wallet components index
    - Export all wallet components from single file
    - Simplify imports across the application

---
+++ frontend/src/components/wallet/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Wallet component index
+ * Export all wallet components
+ */
+
+export { WalletConnectButton } from './WalletConnectButton';
+export { AccountDisplay } from './AccountDisplay';
+export { WalletBalance } from './WalletBalance';
+export { TransactionStatus } from './TransactionStatus';
+export { NetworkSelector } from './NetworkSelector';
